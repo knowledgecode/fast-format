d32386b9306a94f1109debbd5af3a62889e1e5e1
knowledgecode <knowledgecode@gmail.com>
2017-02-05 00:34:12 +0900

    Add format.compile() function

--- fast-format.js
+++ fast-format.js
@@ -1,15 +1,39 @@
 /**
- * @preserve fast-format.js (C) 2015 KNOWLEDGECODE | MIT
+ * @preserve fast-format.js (C) KNOWLEDGECODE | MIT
  */
 (function (global) {
     'use strict';
 
     var f = function (format) {
-        var i, len, argc = arguments.length, v = (format + '').split('%s'), r = argc ? v[0] : '';
-        for (i = 1, len = v.length, argc--; i < len; i++) {
-            r += (i > argc ? '%s' : arguments[i]) + v[i];
+        var argc = arguments.length, v = (argc ? format + '' : '').split('%s'), i = 1, len = v.length, r = v[0];
+        if (argc > len) {
+            argc = len;
+        }
+        while (i < argc) {
+            r += arguments[i] + v[i++];
+        }
+        while (i < len) {
+            r += '%s' + v[i++];
         }
         return r;
+    };
+
+    f.compile = function (format) {
+        var v = (arguments.length ? format + '' : '').split('%s'), len = v.length;
+        return function () {
+            var i = 0, argc = arguments.length, r = v[0];
+            if (argc > len - 1) {
+                argc = len - 1;
+            }
+            while (i < argc) {
+                r += arguments[i] + v[++i];
+            }
+            i++;
+            while (i < len) {
+                r += '%s' + v[i++];
+            }
+            return r;
+        };
     };
 
     if (typeof module === 'object' && typeof module.exports === 'object') {
